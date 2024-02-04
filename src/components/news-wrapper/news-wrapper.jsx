efd172509754d038b10e5d0018295113cf207a68
Juan Lima <juangustavo@gmail.com>
2024-02-04 11:41:20 -0300

    feat: resgatando notícias da API

--- src/components/news-wrapper/news-wrapper.jsx
+++ src/components/news-wrapper/news-wrapper.jsx
@@ -1,23 +1,24 @@
 import "./news-wrapper.css";
-import foto from "../../assets/noticia1.webp";
 import { Link } from "react-router-dom";
 
-export default function NewsWrapper(){
+ function NewsWrapper({ id, img, categoria, data, titulo, noticia }){
     return <>
         <div className="news-wrapper">
             <div className="box-single-news">
                 <div className="img-container">
-                    <img src={foto} alt="Foto Noticia" />
+                    <img src={img} alt="Foto Noticia" />
                 </div>
                 <div className="single-news-content">
                     <div className="news-cat-dt">
-                        <p className="news-category orange"><Link to="/News">Esportes</Link></p>
-                        <p className="news-date">02/02/2024</p>
+                        <p className={`news-category ${categoria.toLowerCase()}`}><Link to="/News">{categoria}</Link></p>
+                        <p className="news-date">{data.slice(0,10)}</p>
                     </div>
-                    <h2><Link to="/News">Hamilton terá salário na Ferrari de R$ 250 milhões</Link></h2>
-                    <p>O piloto Lewis Hamilton terá um salário de £40 milhões por ano (R$ 250 milhões) na Ferrari. A informação é do jornal MailSport. O site ainda diz que Hamilton ...</p>
+                    <h2><Link to="/News">{titulo}</Link></h2>
+                    <p>{noticia.slice(0,175)} ...</p>
                 </div>
             </div>
         </div>
     </>
 }
+
+export default NewsWrapper;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,20 +2,37 @@ import "./home.css";
 import Header from "../../components/header/header.jsx";
 import NewsWrapper from "../../components/news-wrapper/news-wrapper.jsx";
 import Footer from "../../components/footer/footer.jsx";
+import { useState, useEffect } from "react";
 
 
 export default function Home(){
+
+    const [news, setNews] = useState([]);
+    
+    useEffect(() =>{
+        fetch('https://65bf85cb25a83926ab952277.mockapi.io/news/noticias')
+            .then((resp) => resp.json())
+            .then((data) => {
+                setNews(data);
+            })
+            .catch((err) => console.log(err))
+    }, [])    
+
     return <>
         <Header />
 
         <section className="news">
             <div className="center">
-                <NewsWrapper />
-                <NewsWrapper />
-                <NewsWrapper />
-                <NewsWrapper />
-                <NewsWrapper />
-                <NewsWrapper />
+            {news.length > 0 &&
+                    news.map((item) => 
+                    <NewsWrapper
+                    key={item.id} 
+                    img={item.img} 
+                    categoria={item.categoria} 
+                    data={item.data} 
+                    titulo={item.titulo} 
+                    noticia={item.noticia} 
+                    />)}
             </div>
         </section>
 
