23736a1463669c29c712ccabfacea65dbd3cb430
Juan Lima <juangustavo@gmail.com>
2024-02-05 11:07:57 -0300

    refactor: sidebar

--- src/components/news-sidebar/news-sidebar.jsx
+++ src/components/news-sidebar/news-sidebar.jsx
@@ -1,13 +1,10 @@
 import "./news-sidebar.css";
 import { Link } from "react-router-dom";
 
-export default function NewsSidebar({id, img, categoria, titulo, data}){
+export default function NewsSidebar({id, categoria, titulo, data}){
     return<>
 
         <div className="news-img-sidebar-more-news">
-            <div className="more-news-img">
-                <img src={img} alt="Foto Mais Notícias" />
-            </div>
             <div className="more-news-content">
                 <p className={`more-news-content-title ${categoria.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")}Txt`}>{categoria}</p>
                 <p className="more-news-content-news"><Link to={`/News/${id}`}>{titulo}</Link></p>
